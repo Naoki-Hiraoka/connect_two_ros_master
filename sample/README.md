f68825a384dc944965534aa2ecc9329ab7447a4d
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-10-21 11:56:18 +0900

    add sample

---
+++ sample/README.md
@@ -0,0 +1,7 @@
+```
+roslaunch connect_two_ros_master master_side.launch
+```
+
+```
+roslaunch connect_two_ros_master slave_side.launch -p 12345
+```
--- src/connect_two_master.cpp
+++ src/connect_two_master.cpp
@@ -7,7 +7,6 @@
 #include <string>
 #include <sys/ipc.h>
 
-#include <boost/program_options.hpp>
 #include <boost/interprocess/ipc/message_queue.hpp>
 
 struct Data {
@@ -19,34 +18,43 @@ struct Data {
   int size;
 };
 
-void process(int argc, char** argv, std::string this_uri, std::string snd_name, std::string rcv_name, std::vector<std::string>& topics, double rate) {
-  std::string ros_master_uri_str = "ROS_MASTER_URI=" + this_uri;
-  putenv( const_cast<char*>(ros_master_uri_str.c_str()));
+int main(int argc, char** argv) {
+  // init node
   ros::init(argc, argv, "connect_two_master");
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
 
-  boost::interprocess::message_queue::remove((ros::this_node::getName()+snd_name).c_str());
-  sleep(1);
-  ///// get shm
+  std::vector<std::string> topics;
+  if(pnh.hasParam("topics")) pnh.getParam("topics", topics);
+  bool slaveside = false;
+  if(pnh.hasParam("slaveside")) pnh.getParam("slaveside", slaveside);
+
+  // get shm
   boost::interprocess::message_queue sndq(boost::interprocess::open_or_create,
-                                          (ros::this_node::getName()+snd_name).c_str(),
+                                          (ros::this_node::getName()+(slaveside?"SLAVE":"MASTER")).c_str(),
                                           10,
                                           sizeof(Data));
   boost::interprocess::message_queue rcvq(boost::interprocess::open_or_create,
-                                          (ros::this_node::getName()+rcv_name).c_str(),
+                                          (ros::this_node::getName()+(slaveside?"MASTER":"SLAVE")).c_str(),
                                           10,
                                           sizeof(Data));
+  // clear shm. (大昔のデータが残っている場合がある))
+  {
+    Data data;
+    uint64_t rcv_size;
+    unsigned priority;
+    while(rcvq.try_receive(&data, sizeof(Data), rcv_size, priority)) continue;
+  }
 
-  ros::NodeHandle nh;
   Data data;
   std::vector<ros::Subscriber> subs;
   std::unordered_map<std::string, ros::Publisher> pubMap;
 
   for(int i=0;i<topics.size();i++){
-    subs.push_back(nh.subscribe<topic_tools::ShapeShifter>(topics[i], 1, [&](const topic_tools::ShapeShifter::ConstPtr& topic_msg){
+    subs.push_back(nh.subscribe<topic_tools::ShapeShifter>(topics[i], 1, [&,i](const topic_tools::ShapeShifter::ConstPtr& topic_msg){
           // トピック情報の取得
-          std::cerr << 1<<std::endl;
-          if(topics[i].size() > sizeof(data.topicName)) {
-            std::cerr << "topics[i].size()" << topics[i].size() << ">" << "sizeof(data.topicName)" << sizeof(data.topicName) << std::endl;
+          if(topics[i].length()+1 > sizeof(data.topicName)) {
+            std::cerr << "sizeof [" << topics[i] << "] >" << "sizeof(data.topicName)" << sizeof(data.topicName) << std::endl;
             exit(1);
           }
           strcpy(data.topicName, topics[i].c_str());
@@ -72,23 +80,17 @@ void process(int argc, char** argv, std::string this_uri, std::string snd_name, 
           }
           ros::serialization::OStream stream(data.data, data.size);
           topic_msg->write(stream);
-          std::cerr << 2<<std::endl;
-          std::cerr <<data.topicName <<std::endl;
           sndq.try_send(&data, sizeof(Data), 0 );
-                    std::cerr << 3<<std::endl;
         }));
   }
 
-  ros::Rate r(rate);
-  ROS_INFO_STREAM("[connect_two_master] start with " << ros_master_uri_str);
+  ros::Rate r(1000);
 
   while(ros::ok()){
     while(true){
-                std::cerr << 4<<std::endl;
       uint64_t rcv_size;
       unsigned priority;
       if(!rcvq.try_receive(&data, sizeof(Data), rcv_size, priority)) break;
-          std::cerr << 5<<std::endl;
       topic_tools::ShapeShifter shape_shifter;
       shape_shifter.morph(data.md5sum, data.datatype, data.definition, "");
       ros::serialization::OStream stream(data.data, data.size);
@@ -98,45 +100,12 @@ void process(int argc, char** argv, std::string this_uri, std::string snd_name, 
         pubMap[data.topicName] = shape_shifter.advertise(nh, data.topicName, 1);
       }
       pubMap[data.topicName].publish(shape_shifter);
-                std::cerr << 6<<std::endl;
     }
     ros::spinOnce();
     r.sleep();
   }
 
-  exit(EXIT_SUCCESS);
-}
 
-int main(int argc, char** argv) {
-  ///// parse options
-  boost::program_options::options_description op("target_uri_info");
-  op.add_options()
-    ("help,h",                                                      "show help.")
-    ("master_uri,m",boost::program_options::value<std::string>(),   "master hostname or ip.")
-    ("slave_uri,s", boost::program_options::value<std::string>(),   "slave hostname or ip.")
-    ("master_topics,mt", boost::program_options::value<std::vector<std::string>>(), "topics subscribed in master side")
-    ("slave_topics,mt", boost::program_options::value<std::vector<std::string>>(), "topics subscribed in slave side")
-    ;
-  boost::program_options::variables_map argmap;
-  boost::program_options::store(boost::program_options::parse_command_line(argc, argv, op), argmap);
-  boost::program_options::notify(argmap);
-  std::string master_uri = "http://localhost:11311"; // default
-  std::string slave_uri = "http://localhost:12345"; // default
-  std::vector<std::string> master_topics;
-  std::vector<std::string> slave_topics;
-  if( argmap.count("help") ){         std::cerr << op << std::endl; return 1; }
-  if( argmap.count("master_uri") ){   master_uri  = argmap["master_uri"].as<std::string>();   }
-  if( argmap.count("slave_uri") ){    slave_uri   = argmap["slave_uri"].as<std::string>();    }
-  if( argmap.count("master_topics") ){   master_topics  = argmap["master_topics"].as<std::vector<std::string> >();   }
-  if( argmap.count("slave_topics") ){   slave_topics  = argmap["slave_topics"].as<std::vector<std::string> >();   }
-  std::cerr << "master_uri is set as = "  << master_uri   << std::endl;
-  std::cerr << "slave_uri is set as = "   << slave_uri    << std::endl;
 
-  ///// start fork
-  if(fork() == 0) process(argc, argv, slave_uri, "slave", "master",  slave_topics, 1000);
-  process(argc, argv, master_uri, "master", "slave", master_topics, 1000);
-
-  // exit
-  for (int child_cnt = 0; child_cnt < 1; ++child_cnt) { wait(NULL); }
-  return EXIT_SUCCESS;
+  return 0;
 }
