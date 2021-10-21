c3aab5957c0a59531e17f1c265bf15ecc8b958e1
Naoki-Hiraoka <hiraoka@jsk.imi.i.u-tokyo.ac.jp>
2021-10-21 12:57:05 +0900

    fix

--- src/connect_two_master.cpp
+++ src/connect_two_master.cpp
@@ -5,8 +5,10 @@
 #include <topic_tools/shape_shifter.h>
 #include <unordered_map>
 #include <string>
+#include <memory>
 #include <sys/ipc.h>
 
+#include <ros/serialization.h>
 #include <boost/interprocess/ipc/message_queue.hpp>
 
 struct Data {
@@ -28,59 +30,63 @@ int main(int argc, char** argv) {
   if(pnh.hasParam("topics")) pnh.getParam("topics", topics);
   bool slaveside = false;
   if(pnh.hasParam("slaveside")) pnh.getParam("slaveside", slaveside);
+  int datasize = 1000000;
+  if(pnh.hasParam("datasize")) pnh.getParam("datasize", datasize);
+  int queuesize = 10;
+  if(pnh.hasParam("queuesize")) pnh.getParam("queuesize", queuesize);
+  bool reallocate = false;
+  if(pnh.hasParam("reallocate")) pnh.getParam("reallocate", reallocate);
+
+  char buffer[datasize];
+
+  if(reallocate){
+    boost::interprocess::message_queue::remove((ros::this_node::getName()+(slaveside?"SLAVE":"MASTER")).c_str());
+    boost::interprocess::message_queue::remove((ros::this_node::getName()+(slaveside?"MASTER":"SLAVE")).c_str());
+  }
 
   // get shm
   boost::interprocess::message_queue sndq(boost::interprocess::open_or_create,
                                           (ros::this_node::getName()+(slaveside?"SLAVE":"MASTER")).c_str(),
-                                          10,
-                                          sizeof(Data));
+                                          queuesize,
+                                          datasize);
   boost::interprocess::message_queue rcvq(boost::interprocess::open_or_create,
                                           (ros::this_node::getName()+(slaveside?"MASTER":"SLAVE")).c_str(),
-                                          10,
-                                          sizeof(Data));
+                                          queuesize,
+                                          datasize);
   // clear shm. (大昔のデータが残っている場合がある))
   {
-    Data data;
     uint64_t rcv_size;
     unsigned priority;
-    while(rcvq.try_receive(&data, sizeof(Data), rcv_size, priority)) continue;
+    while(rcvq.try_receive(buffer, sizeof(buffer), rcv_size, priority)) continue;
   }
 
-  Data data;
   std::vector<ros::Subscriber> subs;
   std::unordered_map<std::string, ros::Publisher> pubMap;
 
   for(int i=0;i<topics.size();i++){
     subs.push_back(nh.subscribe<topic_tools::ShapeShifter>(topics[i], 1, [&,i](const topic_tools::ShapeShifter::ConstPtr& topic_msg){
-          // トピック情報の取得
-          if(topics[i].length()+1 > sizeof(data.topicName)) {
-            std::cerr << "sizeof [" << topics[i] << "] >" << "sizeof(data.topicName)" << sizeof(data.topicName) << std::endl;
+          if(topics[i].length()+1 +
+             topic_msg->getMD5Sum().length()+1 +
+             topic_msg->getDataType().length()+1 +
+             topic_msg->getMessageDefinition().length()+1 +
+             topic_msg->size()
+             > datasize){
+            std::cerr << "sizeof [" << topics[i] << "] " <<
+              topics[i].length()+1 +
+              topic_msg->getMD5Sum().length()+1 +
+              topic_msg->getDataType().length()+1 +
+              topic_msg->getMessageDefinition().length()+1 +
+              topic_msg->size() << " > " << datasize << std::endl;
             exit(1);
           }
-          strcpy(data.topicName, topics[i].c_str());
-          if(topic_msg->getMD5Sum().size() > sizeof(data.md5sum)) {
-            std::cerr << "topic_msg->getMD5Sum().size()" << topic_msg->getMD5Sum().size() << ">" << "sizeof(data.md5sum)" << sizeof(data.md5sum) << std::endl;
-            exit(1);
-          }
-          strcpy(data.md5sum, topic_msg->getMD5Sum().c_str());
-          if(topic_msg->getDataType().size() > sizeof(data.datatype)) {
-            std::cerr << "topic_msg->getDataType().size()" << topic_msg->getDataType().size() << ">" << "sizeof(data.datatype)" << sizeof(data.datatype) << std::endl;
-            exit(1);
-          }
-          strcpy(data.datatype, topic_msg->getDataType().c_str());
-          if(topic_msg->getMessageDefinition().size() > sizeof(data.definition)) {
-            std::cerr << "topic_msg->getMessageDefinition().size()" << topic_msg->getMessageDefinition().size() << ">" << "sizeof(data.definition)" << sizeof(data.definition) << std::endl;
-            exit(1);
-          }
-          strcpy(data.definition, topic_msg->getMessageDefinition().c_str());
-          data.size = topic_msg->size();
-          if(topic_msg->size() > sizeof(data.data)){
-            std::cerr << "topic_msg->size()" << topic_msg->size() << ">" << "sizeof(data.data)" << sizeof(data.data) << std::endl;
-            exit(1);
-          }
-          ros::serialization::OStream stream(data.data, data.size);
-          topic_msg->write(stream);
-          sndq.try_send(&data, sizeof(Data), 0 );
+          int idx=0;
+          strcpy(buffer+idx,topics[i].c_str()); idx+=topics[i].length()+1;
+          strcpy(buffer+idx,topic_msg->getMD5Sum().c_str()); idx+=topic_msg->getMD5Sum().length()+1;
+          strcpy(buffer+idx,topic_msg->getDataType().c_str()); idx+=topic_msg->getDataType().length()+1;
+          strcpy(buffer+idx,topic_msg->getMessageDefinition().c_str()); idx+=topic_msg->getMessageDefinition().length()+1;
+          ros::serialization::OStream stream((uint8_t*)buffer+idx, topic_msg->size());
+          topic_msg->write(stream); idx+=topic_msg->size();
+          sndq.try_send(buffer, idx, 0 );
         }));
   }
 
@@ -90,16 +96,21 @@ int main(int argc, char** argv) {
     while(true){
       uint64_t rcv_size;
       unsigned priority;
-      if(!rcvq.try_receive(&data, sizeof(Data), rcv_size, priority)) break;
+      if(!rcvq.try_receive(buffer, datasize, rcv_size, priority)) break;
+      int idx=0;
+      std::string topicName=buffer+idx; idx+=topicName.length()+1;
+      std::string MD5Sum=buffer+idx; idx+=MD5Sum.length()+1;
+      std::string DataType=buffer+idx; idx+=DataType.length()+1;
+      std::string MessageDefinition=buffer+idx; idx+=MessageDefinition.length()+1;
       topic_tools::ShapeShifter shape_shifter;
-      shape_shifter.morph(data.md5sum, data.datatype, data.definition, "");
-      ros::serialization::OStream stream(data.data, data.size);
-      shape_shifter.read(stream);
-      if(pubMap.find(data.topicName)==pubMap.end()){
-        std::cerr <<data.topicName <<std::endl;
-        pubMap[data.topicName] = shape_shifter.advertise(nh, data.topicName, 1);
+      shape_shifter.morph(MD5Sum, DataType, MessageDefinition, "");
+      ros::serialization::OStream ostream((uint8_t*)buffer+idx, rcv_size-idx);
+      shape_shifter.read(ostream);
+      if(pubMap.find(topicName)==pubMap.end()){
+        std::cerr <<topicName <<std::endl;
+        pubMap[topicName] = shape_shifter.advertise(nh, topicName, 1);
       }
-      pubMap[data.topicName].publish(shape_shifter);
+      pubMap[topicName].publish(shape_shifter);
     }
     ros::spinOnce();
     r.sleep();
